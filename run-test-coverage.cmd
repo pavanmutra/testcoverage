5925c42624d2848c06a270487a916491f23d10e9
pavan reddy <pavankumarmutra@gmail.com>
2020-10-07 18:34:20 +0530

    Update run-test-coverage.cmd

--- run-test-coverage.cmd
+++ run-test-coverage.cmd
@@ -1,6 +1,8 @@
+dotnet tool list -g 
 dotnet test calculation.tests/calculation.tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet build-server shutdown
 dotnet tool install --global dotnet-sonarscanner --version 4.8.0
+dotnet tool list -g 
 dotnet sonarscanner begin /k:"calculation" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="calculation.tests\coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs"
 dotnet build
 dotnet sonarscanner end
